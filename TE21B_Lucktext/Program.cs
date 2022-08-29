931b0e96fb767659945afbf43ffacad3f8c8f981
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2022-08-29 12:56:03 +0200

    Initial commit

---
+++ TE21B_Lucktext/Program.cs
@@ -0,0 +1,15 @@
+﻿// Console.WriteLine("Hej NTI");
+// Console.WriteLine("Hej TE21B");
+
+string name;
+
+// name = "Micke";
+// name = "Anna";
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine($"VÄLKOMMEN {name} !");
+
+
+Console.ReadLine();
